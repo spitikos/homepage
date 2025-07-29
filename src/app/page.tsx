128fa1bd2b076104285be8fd623a0a2a8ee24155
Ethan Lee <ethantlee21@gmail.com>
2025-07-28 23:24:56 -0400

    add test stats api connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <main>HOME</main>;
+  const [stat, setStat] = useState<string>();
+
+  useEffect(() => {
+    const statSource = new EventSource("https://pi.taehoonlee.dev/api/stats");
+
+    statSource.onmessage = (event) => {
+      setStat(event.data);
+    };
+
+    statSource.onerror = (err) => {
+      console.error("SSE error:", err);
+      statSource.close();
+    };
+
+    return () => {
+      statSource.close();
+    };
+  }, []);
+
+  return (
+    <main>
+      <p>CPU: {JSON.parse(stat ?? "{}").temperature?.cpuTemperature}</p>
+    </main>
+  );
 }
