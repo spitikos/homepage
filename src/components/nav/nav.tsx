8c42ad536aee8cf03ff8fdbc1769491d978aa08b
Ethan Lee <ethantlee21@gmail.com>
2025-08-10 11:29:58 -0500

    fix: nav breadcrumb

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Fragment } from "react";
@@ -9,16 +10,18 @@ const Header = () => {
   const paths = pathname.split("/").slice(1);
 
   return (
-    <header className="fixed top-0 left-0 flex items-center px-5 py-4">
-      <span className="leading-none">
-        <Link href="/">PI</Link>
+    <header className="fixed top-0 left-0 flex items-center px-5 h-12 text-sm">
+      <div className="flex">
+        <Link href="/" className="relative rounded overflow-hidden">
+          <Image src="/logo.svg" alt="logo" width={20} height={20} />
+        </Link>
         {paths.map((path, i) => (
           <Fragment key={i}>
             <span className="mx-2 text-secondary">/</span>
             <Link href={`/${path}`}>{path.toUpperCase()}</Link>
           </Fragment>
         ))}
-      </span>
+      </div>
     </header>
   );
 };
